dce3024c5681aeff88338fe90ad020e3944b8bad
TejaKumar123 <inceptioncom414@gmail.com>
2024-10-03 22:11:50 +0530

    navbar adding

---
+++ src/components/Navbar/Navbar2.jsx
@@ -0,0 +1,16 @@
+
+
+const Navbar2 = () => {
+	return (
+		<div className="w-[100%] h-[120px] flex flex-row items-center justify-start pl-[100px] gap-[10px] bg-[url('images/infinimove/14.jpeg')] bg-[length:100%_100%]">
+			<img src="/images/infinimove/Kushma Nursing College - Logo.png" className="w-[80px]" />
+			<div className="w-auto h-[100%] flex flex-col items-start justify-center">
+				<p className="text-[red] text-[125%]">Kushma Eduction Society,s</p>
+				<p className="text-[210%]">KUSHMA COLLEGE OF PHARMACY</p>
+				<p className="text-[83%] text-[red]">Affiliated to Rajiv Gandhi University of Health sciences (RGUHS), Bangalore and the Board of Examination Authority, Drugs Control Department, Govt. of Karnataka, Bangalore.</p>
+			</div>
+		</div>
+	)
+}
+
+export default Navbar2
